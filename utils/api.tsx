bc91b45beb9f7b0a774a4cd12f26ce88dcb3e2d8
Olalla <olalla.lopeznavarro-casas@iesvalleinclan.es>
2025-06-08 17:18:31 +0200

    si

--- utils/api.tsx
+++ utils/api.tsx
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 // utils/api.ts (o donde tengas tu instancia de Axios)
 const api = axios.create({
-  baseURL: 'http://192.168.0.16:7999',// <-- Casa Villanueva
+  baseURL: 'https://servitechapi.fly.dev',// <-- Casa Villanueva
+//  baseURL: 'http://192.168.0.16:7999',// <-- Casa Villanueva
   //baseURL:'http://192.168.1.73:8000', // <-- Casa Guille
 //baseURL:'http://172.20.10.2:7999',
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
